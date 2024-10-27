a4edec6f6dfd2a41c5f37bdc4edcfaa2f8c3badd
SELVA GANESH G <119717606+Gans19@users.noreply.github.com>
2024-10-27 10:35:34 +0530

    Updated Socket.io
    Updated Socket.io

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,13 +15,9 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			// const socket = io("https://chat-app-yt.onrender.com", {
-			// 	query: {
-			// 		userId: authUser._id,
-			// 	},
-			// });
+		
 
-			const socket = io("http://localhost:3000", {
+			const socket = io("https://real-chat-p248.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
